19d12652d92d2515800c89c08cc7e24881cda26a
Laura <133364744+laurajhagsuUGAVGHA@users.noreply.github.com>
2023-08-18 16:33:24 -0300

    Initial commit

---
+++ questoes.js
@@ -0,0 +1,52 @@
+
+
+
+export default [
+    {
+      question: "Os planetas do Sistema Solar podem ser classificados conforme a sua composição. Com base nessa classificação, pode-se afirmar que são planetas rochosos:",
+      answers: [
+       
+        { option: "Terra, Marte, Urano e Netuno.", correct: false},
+        { option: " Terra, Marte, Saturno e Plutão.", correct: false },
+        { option: "Vênus, Marte, Saturno e Urano.", correct: false },
+        { option: "Mercúrio, Vênus, Terra e Marte.", correct: true },
+      ],
+    },
+    {
+      question: "Os planetas gasosos são compostos por gases, como hidrogênio e hélio. Qual o nome do maior planeta gasoso do Sistema Solar?",
+      answers: [
+        { option: "Marte", correct: false },
+        { option: "Júpiter", correct: true },
+        { option: "Saturno", correct: false },
+        { option: "Urano", correct: false },
+        
+      ],
+    },
+    {
+      question: "O planeta Terra realiza vários movimentos, sendo os dois principais o de rotação, realizado em torno de si mesmo, e o movimento realizado em torno do Sol, sendo corretamente chamado de:",
+      answers: [
+        { option: " rotação.", correct: false },
+        { option: "mutação.", correct: false },
+        { option: "movimentação.", correct: false },
+        { option: "translação.", correct: true },
+      ],
+    },
+    {
+      question: "O movimento de rotação realizado pelo planeta Terra tem como consequência principal a",
+      answers: [
+        { option: "sucessão dos dias e das noites.", correct: true },
+        { option: "ocorrência das fases da Lua.", correct: false },
+        { option: "definição das temperaturas.", correct: false },
+        { option: " divisão das estações do ano.", correct: false },
+      ],
+    },
+    {
+        question: "Como é corretamente denominada a teoria que justifica a formação do Sistema Solar?",
+        answers: [
+          { option: "Teoria celular", correct: false },
+          { option: "Teoria da nebulosa", correct: true },
+          { option: "Teoria da deriva", correct: false },
+          { option: " Teoria das cordas", correct: false},
+        ],
+      },
+  ];
